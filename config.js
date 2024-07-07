2245f38b26c50adc71fd1c92cd52821912a8915b
Maxwellmd1 <157256291+Maxwellmd1@users.noreply.github.com>
2024-07-07 09:32:15 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ global.audio = "";
 global.video = "";
 global.port = process.env.PORT;
 global.appUrl = process.env.APP_URL || "";
-global.email = "ngoyaibraah@gmail.com";
+global.email = "emmanuelagbonye@gmail.com";
 global.location = "Bumgoma,Kenya";
 global.mongodb = process.env.MONGODB_URI || "mongodb+srv://giftedte:SER3spXjIJSOwrPT@cluster0.ni61idp.mongodb.net/?retryWrites=true&w=majority";
 global.allowJids = process.env.ALLOW_JID || "null";
@@ -14,14 +14,14 @@ global.blockJids = process.env.BLOCK_JID || "null";
 global.DATABASE_URL = process.env.DATABASE_URL || "postgres://giftedtech:ZMIkfrL3rHsvmfyGboVXKkeyoM0FPl6z@dpg-cpp83jlds78s73e39m10-a.oregon-postgres.render.com/giftedtech";
 global.BUTTONS = process.env.BUTTON_MODE || "1";
 global.timezone = process.env.TIME_ZONE || process.env.TIME_ZONE || "Africa/Nairobi";
-global.github = process.env.GITHUB || "https://github.com/ibrahimaitech/BLACK-PANTHER-XMD";
-global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029VaZuGSxEawdxZK9CzM0Y";
-global.website = process.env.GURL || "https://youtube.com/@ibrahimaitech";
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/c5b2b527a08a0b21392e0.jpg";
-global.devs = "254739937062,254710772666";
-global.sudo = process.env.SUDO_NUMBERS || "254739937062,254710772666";
-global.sudo = process.env.SUDO || "254739937062,254710772666";
-global.owner = process.env.OWNER_NUMBER || "254739937062,254710772666";
+global.github = process.env.GITHUB || "https://wa.me/qr/MY2JIUSBX5FJD1";
+global.gurl = process.env.GURL || "https://wa.me/qr/MY2JIUSBX5FJD1";
+global.website = process.env.GURL || "https://wa.me/qr/MY2JIUSBX5FJD1";
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/ac60b32f3fcb180747985.jpg";
+global.devs = "2349120730021,254710772666";
+global.sudo = process.env.SUDO_NUMBERS || "2349164661375";
+global.sudo = process.env.SUDO || "2349164661375";
+global.owner = process.env.OWNER_NUMBER || "2349120730021,2349164661375";
 global.style = process.env.STYLE || "1";
 global.gdbye = process.env.GOODBYE || "false";
 global.wlcm = process.env.WELCOME || "false";
@@ -30,18 +30,18 @@ global.disablepm = process.env.DISABLE_PM || "true";
 global.disablegroup = process.env.DISABLE_GROUPS || "false",
 global.MsgsInLog = process.env.MSGS_IN_LOG || "true";
 global.userImages = process.env.MENU_VIDEOS || "";
-global.waPresence = process.env.WAPRESENCE || "available";
-global.userImages = process.env.USER_IMAGES || "https://telegra.ph/file/9e038e5890def3cc7aca7.jpg";
+global.waPresence = process.env.WAPRESENCE || "unavailable";
+global.userImages = process.env.USER_IMAGES || "https://telegra.ph/file/a1f712400b07d9193c5da.jpg";
 global.readcmds = process.env.READ_COMMAND || "true";
 global.readmessage = process.env.READ_MESSAGE || "false";
-global.readmessagefrom = process.env.READ_MESSAGE_FROM || "254739937062,254710772666";
+global.readmessagefrom = process.env.READ_MESSAGE_FROM || "254739937062,";
 global.read_status = process.env.AUTO_READ_STATUS || "false";
 global.save_status = process.env.AUTO_SAVE_STATUS || "false";
 global.save_status_from = process.env.SAVE_STATUS_FROM || "";
-global.read_status_from = process.env.READ_STATUS_FROM || "254739937062,254710772666";
+global.read_status_from = process.env.READ_STATUS_FROM || "2349120730021,254710772666";
 
 global.api_smd = "https://api-smd.onrender.com";
-global.scan = "https://whatsapp.com/channel/0029VaZuGSxEawdxZK9CzM0Y";
+global.scan = "https://whatsapp.com/channel/0029VaZuGSxEawdxZK9C";
 
 global.SESSION_ID =
   process.env.SESSION_ID ||
@@ -51,12 +51,12 @@ module.exports = {
   HANDLERS: process.env.PREFIX || "⚡",
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "4.0.0",
-  caption: process.env.CAPTION ||" 𝘽𝙇𝘼𝘾𝙆-𝙋𝘼𝙉𝙏𝙃𝙀𝙍-𝙈𝘿",
-  author: process.env.STICKER_AUTHOR || "𝘽𝙇𝘼𝘾𝙆-𝙋𝘼𝙉𝙏𝙃𝙀𝙍-𝙈𝘿",
-  packname: process.env.STICKER_NAME || "🐯",
-  botname: process.env.BOT_NAME || "𝘽𝙇𝘼𝘾𝙆-𝙋𝘼𝙉𝙏𝙃𝙀𝙍-𝙈𝘿",
-  ownername: process.env.OWNER_NAME || "𝑰𝒃𝒓𝒂𝒉𝒊𝒎 𝑨𝒅𝒂𝒎𝒔",
-  errorChat: process.env.ERROR_CHAT || "",
+  caption: process.env.CAPTION ||"𖣘𝑴𝑨𝑿𝑾𝑬𝑳𝑳-𝑴𝑫♡●",
+  author: process.env.STICKER_AUTHOR || "𖣘𝑴𝑨𝑿𝑾𝑬𝑳𝑳-𝑴𝑫♡●",
+  packname: process.env.STICKER_NAME || "〽️",
+  botname: process.env.BOT_NAME || "𖣘𝑴𝑨𝑿𝑾𝑬𝑳𝑳-𝑴𝑫♡●",
+  ownername: process.env.OWNER_NAME || "Maxwell",
+  errorChat: process.env.ERROR_CHAT || "💔",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "neLbXXrp8bSDcohnp1CW5UEa",
   OPENAI_API_KEY: process.env.OPENAI_API_KEY || "",
